1112d48747128991205796ba41c579225b475b67
Myriamp18 <132544287+Myriamp18@users.noreply.github.com>
2024-01-16 09:31:58 -0600

    Create Inicio.js

---
+++ src/Componentes/Inicio.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Silos from './Silos/Silos'
+
+
+function Inicio() {
+    return(
+    
+        <div>
+        
+ 
+    <Silos/>
+
+    </div>
+        
+    )
+}
+
+export default Inicio
