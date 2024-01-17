d06f82b40d4c0db1667762d7a3d6b59c1e82386c
Myriamp18 <132544287+Myriamp18@users.noreply.github.com>
2024-01-17 14:13:41 -0600

    Update FrmSilos.js

--- frontend/src/Componentes/Silos/FrmSilos.js
+++ frontend/src/Componentes/Silos/FrmSilos.js
@@ -1,93 +1,120 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {faPlus} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './FrmSilos.css'
-import Form from 'react-bootstrap/Form';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios'
 
 
 function FrmSilos() {
 
-    const [show, setShow] = useState(false);
-    const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false); 
+  const [values, setValues] = useState({
+    fecha: '',
+    silo1: "",
+    silo2: "",
+    silo3: "",
+    silo4: "",
+    silo5: ""
+
+  })
+
+  const navigate = useNavigate()
+
+  const handleSubmit =(e) => {
+    e.preventDefault()
+    axios.post('http://localhost:8081/create', values)
+    .then(res => {
+      console.log(res);
+      // Optionally, you can navigate to a different page or update the UI
+      navigate('/Inicio'); // Example: Navigate to the home page
+    })
+    .catch(err => console.log(err));
+};
+
+    
 
     return(
-     <div>
-      <Button variant="danger" size="lg"   onClick={handleShow}  >
-      <FontAwesomeIcon icon={faPlus}/> <strong >Insertar </strong>
-      </Button>
-
-      <Modal show={show} onHide={handleClose}  animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-          <div className="headert">
-          <div className="texto">Minerales Silos</div>
-          <div className="linea"></div>
-        </div>
-         </Modal.Title>
-
-        </Modal.Header>
-        <Modal.Body>
-        <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Fecha:</Form.Label>
-              <Form.Control
+     <div className="d-flex align-items-center flex-column mt-3">
+      <h1>Insertar Silos</h1>
+        <form className="w-50" onSubmit={handleSubmit} >
+            <div class="mb-3 mt-3">
+              <label form='fecha' class="form-label"> Fecha:</label>
+              <input
                 type="date"  
+                class="form-control"
+                id='date'
+                placeholder='Insertar Cantidad'
+                name='fecha'
+                onChange={(e) => setValues({...values, fecha: e.target.value})}
               />
-            </Form.Group>
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlInput2">
-              <Form.Label>Silo 1:</Form.Label>
-              <Form.Control type="text"  placeholder="cantidad"  />
-            </Form.Group>
-
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlInput2">
-              <Form.Label>Silo 2:</Form.Label>
-              <Form.Control type="text"  placeholder="cantidad"  />
-            </Form.Group>
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlInput2">
-              <Form.Label>Silo 3:</Form.Label>
-              <Form.Control type="text"  placeholder="cantidad"  />
-            </Form.Group>
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlInput2">
-              <label>Silo 4:</label>
-              <Form.Control type="text"  placeholder="cantidad"  />
-            </Form.Group>
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlInput2">
-              <Form.Label>Silo 5:</Form.Label>
-              <Form.Control type="text"  placeholder="cantidad"  />
-            </Form.Group>
-
-
-
-
-          </Form>
-        
-        </Modal.Body>
-        <Modal.Footer>
-        <div className="btn-container">
-            <button className="BTN"  >GUARDAR</button>
             </div>
+
+            <div class="mb-3">
+              <label form='text' class="form-label"> Silo 1:</label>
+              <input
+               type="text"  
+               class="form-control"
+               id='silo1'
+               placeholder='Insertar Cantidad'  
+               name='silo1'
+               onChange={(e) => setValues({...values, silo1: e.target.value})}/>
+            </div>
+
+
+            <div class="mb-3">
+              <label form='text' class="form-label"> Silo 2:</label>
+              <input
+               type="text"  
+               class="form-control"
+               id='silo2'
+               placeholder='Insertar Cantidad'  
+               name='silo2'
+               onChange={(e) => setValues({...values, silo2: e.target.value})}/>
+            </div>
+
+            <div class="mb-3">
+              <label form='text' class="form-label"> Silo 3:</label>
+              <input
+               type="text"  
+               class="form-control"
+               id='silo3'
+               placeholder='Insertar Cantidad'  
+               name='silo3'
+               onChange={(e) => setValues({...values, silo3: e.target.value})}/>
+            </div>
+
+            <div class="mb-3">
+              <label form='text' class="form-label"> Silo 4:</label>
+              <input
+               type="text"  
+               class="form-control"
+               id='silo4'
+               placeholder='Insertar Cantidad'  
+               name='silo4'
+               onChange={(e) => setValues({...values, silo4: e.target.value})}/>
+            </div>
+
+            <div class="mb-3">
+              <label form='text' class="form-label"> Silo 5:</label>
+              <input
+               type="text"  
+               class="form-control"
+               id='silo5'
+               placeholder='Insertar Cantidad'  
+               name='silo5'
+               onChange={(e) => setValues({...values, silo5: e.target.value})}/>
+            </div>
+
+            <div className="btn-container">
+            <button type="submit" className="BTN"  >GUARDAR</button>
+            </div>
+
+
+          </form>
+        
+       
+        
          
-        </Modal.Footer>
-      </Modal>
-    
+        
 
     </div>
         
