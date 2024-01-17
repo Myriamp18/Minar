725f37522bed7c2802ecd5b315b37ca4e0845201
Myriamp18 <132544287+Myriamp18@users.noreply.github.com>
2024-01-17 14:13:38 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,31 @@ import './App.css';
 import Login from './Componentes/Login'
 import { BrowserRouter as Router, Routes , Route} from 'react-router-dom'
 import Inicio from './Componentes/Inicio'
+import FrmSilos from './Componentes/Silos/FrmSilos';
+
+import ModificarSilos from './Componentes/Silos/ModificarSilos';
 
 
 
 
 function App() {
   return (
-    <div>
+   
     <Router>
     
        <Routes>
        <Route path="/" element={<Login />}/>
        <Route path="/Inicio" element={<Inicio/>}/>
+       <Route path="/create" element={<FrmSilos/>}/>
+       <Route path="/update/:id" element={<ModificarSilos/>}/>
+       
        
          
        
        </Routes>
     </Router>
      
-   </div>
+  
      
     
   );
