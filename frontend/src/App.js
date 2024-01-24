6e981498cf635ad64a74ef39454ba69f444f3c12
Myriamp18 <132544287+Myriamp18@users.noreply.github.com>
2024-01-24 14:50:31 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,10 @@ import FrmPMoler from './Componentes/ConcPMoler/FrmPMoler';
 import ModificarCPMoler from './Componentes/ConcPMoler/ModificarCPMoler';
 import ConcPMoler from './Componentes/ConcPMoler/ConcPMoler';
 import Menu from './Componentes/Menu'
-
-
+import Silos from './Componentes/Silos/Silos';
+import Seleccion from './Componentes/Seleccion/Seleccion';
+import GranoBandas from './Componentes/GranoBandas/GranoBandas';
+import ReporteDiario from './Componentes/ReporteDiario/ReporteDiario'
 
 
 
@@ -26,28 +28,34 @@ function App() {
     <Router>
     
    
-      
-       
-      
-   
        <Routes>
        <Route path="/" element={<Login />}/>
        <Route path="/Inicio" element={<Inicio/>}/>
+       <Route path="/diario" element={<ReporteDiario/>}/>
+
+
        /////////SILOS///////
        <Route path="/create" element={<FrmSilos/>}/>
        <Route path="/update/:id" element={<ModificarSilos/>}/>
+       <Route path="/silos" element={<Silos/>}/>
+
        ////USUARIO/////////
        <Route path="/createusuario" element={<InsertarUsuario/>}/>
+
        /////////GRANOBANDAS/////
        <Route path="/creategrano" element={<FrmBandas/>}/>
        <Route path="/updategrano/:id" element={<ModificarGrano/>}/>
+       <Route path="/granobandas" element={<GranoBandas/>}/>
+
        ////////SELECCION/////
        <Route path="/createseleccion" element={<FrmSeleccion/>}/>
        <Route path="/updateseleccion/:id" element={<ModificarSeleccion/>}/>
+       <Route path="/seleccion" element={<Seleccion/>}/>
+
        ///////////CONC.P/MOLER/////////7
        <Route path="/createconcpmoler" element={<FrmPMoler/>}/>
        <Route path="/updateconcpmoler/:id" element={<ModificarCPMoler/>}/>
-      
+       <Route path="/concpmoler" element={<ConcPMoler/>}/>
        
          
        
